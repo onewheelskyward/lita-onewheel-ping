6c25377e4246564bc49dc4ccfc038497d4d03118
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-14 00:58:25 -0700

    Hmmyeaaaah.

--- spec/lita/handlers/onewheel_ping_spec.rb
+++ spec/lita/handlers/onewheel_ping_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Lita::Handlers::OnewheelPing, lita_handler: true do
   it 'should pong' do
     send_message 'ping'
-    expect(replies.last).to include('https*://')
+    expect(replies.last).to include('http')
   end
 end
