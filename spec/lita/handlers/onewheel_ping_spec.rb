34693fe905af52f33970944e3f7e00c3c23850d9
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-14 00:57:06 -0700

    Allow https in test.

--- spec/lita/handlers/onewheel_ping_spec.rb
+++ spec/lita/handlers/onewheel_ping_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Lita::Handlers::OnewheelPing, lita_handler: true do
   it 'should pong' do
     send_message 'ping'
-    expect(replies.last).to include('http://')
+    expect(replies.last).to include('https*://')
   end
 end
